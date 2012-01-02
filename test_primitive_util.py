e7c9deae38ca5e9bdada2e58eadfad582d6e736c
Rob Renaud <rrenaud@gmail.com>
2012-01-02 00:25:27 -0500

    added new, more compact serialization

--- test_primitive_util.py
+++ test_primitive_util.py
@@ -64,5 +64,40 @@ class NestedClassPrimTest(unittest.TestCase):
         again_prim_a = returned_a.to_primitive_object()
         self.assertEquals(prim_a, again_prim_a)
 
+class ListSlotPrimTest(unittest.TestCase):
+    def test(self):
+        class F(primitive_util.ListSlotPrimitiveConversion):
+            __slots__ = ('f1', 'f2')
+
+            def __init__(self):
+                self.f1 = 0
+                self.f2 = 0
+
+        class G(primitive_util.ListSlotPrimitiveConversion):
+            __slots__ = ('g1', 'g2')
+            
+            def __init__(self):
+                self.g1 = F()
+                self.g2 = F()
+
+        self.assertEquals(primitive_util.slot_index_count(int()), 1)
+        self.assertEquals(primitive_util.slot_index_count(F()), 2)
+        self.assertEquals(primitive_util.slot_index_count(G()), 4)
+
+        g = G()
+        g.g1.f1 = 1
+        g.g1.f2 = 2
+        g.g2.f1 = 3
+        g.g2.f2 = 5
+        prim_g = g.to_primitive_object()
+        self.assertEquals(prim_g, [1,2,3,5])
+        new_g = G()
+        new_g.from_primitive_object(prim_g)
+        self.assertEquals(new_g.g1.f1, 1)
+        self.assertEquals(new_g.g1.f2, 2)
+        self.assertEquals(new_g.g2.f1, 3)
+        self.assertEquals(new_g.g2.f2, 5)
+
+
 if __name__ == '__main__':
     unittest.main()
