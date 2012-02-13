169c082c48cd6d6944d42bf300d39e0f26fcc2d5
Timothy Lee Styer <tlstyer@gmail.com>
2012-02-13 01:00:45 -0800

    scrape from councilroom.com ; timeout after 30 seconds ; retry up to 3 times ; be kind of verbose

--- scrape_leaderboard.py
+++ scrape_leaderboard.py
@@ -33,30 +33,23 @@ def get_date_of_current_isotropic_leaderboard():
     headers = dict(res.getheaders())
     conn.close()
 
-    # just after midnight Pacific time, GMT will have the same calendar date as Pacific time
-    # so, we can ignore the hour, minute, and second
-    return datetime.datetime.strptime(headers['last-modified'], '%a, %d %b %Y %H:%M:%S %Z').date()
+    if res.status == 200:
+        # just after midnight Pacific time, GMT will have the same calendar date as Pacific time
+        # so, we can ignore the hour, minute, and second
+        return datetime.datetime.strptime(headers['last-modified'], '%a, %d %b %Y %H:%M:%S %Z').date()
 
-def save_file(gzipped_data, date):
-    f = gzip.GzipFile(fileobj=StringIO.StringIO(gzipped_data))
-    data = f.read()
-    f.close()
-
-    data = bz2.compress(data)
-
-    f = open(output_directory + str(date) + '.html.bz2', 'w')
-    f.write(data)
-    f.close()
-
+# originally from http://bggdl.square7.ch/leaderboard/
 def scrape_leaderboard_from_online_cache(date):
-    conn = httplib.HTTPConnection('bggdl.square7.ch')
-    conn.request('GET', '/leaderboard/leaderboard-' + str(date) + '.html', headers={'Accept-Encoding': 'gzip'})
+    conn = httplib.HTTPConnection('councilroom.com', timeout=30)
+    conn.request('GET', '/static/leaderboard/' + str(date) + '.html.bz2')
     res = conn.getresponse()
-    gzipped_data = res.read()
+    data = res.read()
     conn.close()
 
     if res.status == 200:
-        save_file(gzipped_data, date)
+        f = open(output_directory + str(date) + '.html.bz2', 'w')
+        f.write(data)
+        f.close()
         return True
     elif res.status == 404:
         # doesn't exist, but pretend we got it
@@ -65,14 +58,23 @@ def scrape_leaderboard_from_online_cache(date):
         return False
 
 def scrape_leaderboard_from_isotropic(date):
-    conn = httplib.HTTPConnection('dominion.isotropic.org')
+    conn = httplib.HTTPConnection('dominion.isotropic.org', timeout=30)
     conn.request('GET', '/leaderboard/', headers={'Accept-Encoding': 'gzip'})
     res = conn.getresponse()
     gzipped_data = res.read()
     conn.close()
 
     if res.status == 200:
-        save_file(gzipped_data, date)
+        f = gzip.GzipFile(fileobj=StringIO.StringIO(gzipped_data))
+        data = f.read()
+        f.close()
+
+        data = bz2.compress(data)
+
+        f = open(output_directory + str(date) + '.html.bz2', 'w')
+        f.write(data)
+        f.close()
+
         return True
     else:
         return False
@@ -80,20 +82,43 @@ def scrape_leaderboard_from_isotropic(date):
 def main():
     utils.ensure_exists(output_directory)
 
-    one_day_delta = datetime.timedelta(1)
-    date = get_date_of_last_cached_leaderboard() + one_day_delta
-    date_of_current_isotropic_leaderboard = get_date_of_current_isotropic_leaderboard()
-    success = True
+    date_of_last_cached_leaderboard = get_date_of_last_cached_leaderboard()
+    print 'date of the last cached leaderboard is', date_of_last_cached_leaderboard
 
-    while success and date <= date_of_current_isotropic_leaderboard:
+    date_of_current_isotropic_leaderboard = get_date_of_current_isotropic_leaderboard()
+    if date_of_current_isotropic_leaderboard is None:
+        print 'could not determine the date of the current isotropic leaderboard, so please try again later'
+        return
+    print 'date of the current isotropic leaderboard is', date_of_current_isotropic_leaderboard
+
+    one_day_delta = datetime.timedelta(1)
+    date = date_of_last_cached_leaderboard + one_day_delta
+    success = True
+    num_times_unsuccessful_in_a_row = 0
+
+    while date <= date_of_current_isotropic_leaderboard:
+        print
         print date
 
         if date == date_of_current_isotropic_leaderboard:
+            print 'scraping from isotropic'
             success = scrape_leaderboard_from_isotropic(date)
         else:
+            print 'scraping from online cache'
             success = scrape_leaderboard_from_online_cache(date)
 
-        date += one_day_delta
+        if success:
+            print 'successful'
+            num_times_unsuccessful_in_a_row = 0
+            date += one_day_delta
+        else:
+            num_times_unsuccessful_in_a_row += 1
+            print 'unsuccessful', num_times_unsuccessful_in_a_row, 'time(s) in a row'
+            if num_times_unsuccessful_in_a_row < 3:
+                print 'retrying...'
+            else:
+                print 'reached max unsuccessful attempts in a row, so please try again later'
+                break
 
 if __name__ == '__main__':
     main()
