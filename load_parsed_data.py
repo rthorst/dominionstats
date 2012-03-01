9349fc12b040fc7c9123657ebed8a9e14e5b2bd6
David Lu <davidvlu@gmail.com>
2012-02-29 23:05:11 -0600

    Remove parsed data when loaded correctly

--- load_parsed_data.py
+++ load_parsed_data.py
@@ -9,6 +9,39 @@ import argparse
 import utils
 
 parser = utils.incremental_date_range_cmd_line_parser()
+find_id = re.compile('game-.*.html')
+
+def process_file(filename, incremental, games_table):
+    yyyymmdd = filename[:8]
+
+    if incremental:
+        contents = open('parsed_out/' + filename, 'r').read()
+        if contents.strip() == '[]':
+            print "empty contents (make parser not dump empty files?)", \
+                  filename
+            return
+
+        assert find_id.search(contents), (
+            'could not get id from %s in file %s' % (contents[:100], filename))
+
+        found_all = True
+        for match in find_id.finditer(contents):
+            g_id = match.group(0)
+            query = {'_id': g_id}
+            if games_table.find(query).count():
+                continue
+            else:
+                found_all = False
+        if found_all:
+            print "Found all games in DB, deleting file"
+            os.system('rm parsed_out/%s'%filename)
+            return
+    
+    cmd = ('mongoimport -h localhost parsed_out/%s -c '
+           'games --jsonArray' % filename)
+    print cmd
+    os.system(cmd)
+
 
 def main():
     args = parser.parse_args()
@@ -17,40 +50,13 @@ def main():
     games_table.ensure_index('supply')
     data_files_to_load = os.listdir('parsed_out')
     data_files_to_load.sort()
-    find_id = re.compile('game-.*.html')
-    done = set()
+
     for fn in data_files_to_load:
         yyyymmdd = fn[:8]
-        print yyyymmdd
         if not utils.includes_day(args, yyyymmdd):
             print 'skipping', fn, 'because not in range'
             continue
-
-        if args.incremental:
-            if yyyymmdd in done:
-                print 'skipping', fn, 'because done'
-                continue
-            contents = open('parsed_out/' + fn, 'r').read(100)
-            if contents.strip() == '[]':
-                print "empty contents (make parser not dump empty files?)", \
-                      fn
-                continue
-            first_game_id_match = find_id.search(contents)
-            assert first_game_id_match is not None, (
-                'could not get id from %s in file %s' % (contents, fn))
-            first_game_id = first_game_id_match.group(0)
-            query = {'_id': first_game_id}
-            if games_table.find(query).count():
-                done.add(yyyymmdd)
-                print 'skipping', yyyymmdd, 'and marking as done'
-                continue
-            else:
-                print first_game_id, str(query), 'not in db, importing'
-        
-        cmd = ('mongoimport -h localhost parsed_out/%s -c '
-               'games --jsonArray' % fn)
-        print cmd
-        os.system(cmd)
+        process_file(fn, args.incremental, games_table)
 
 if __name__ == '__main__':
     main()
