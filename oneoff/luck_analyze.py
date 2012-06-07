600047addfc61a9aa1ee7d53cc454ca01eef60d8
Rob Renaud <rrenaud@gmail.com>
2012-06-07 08:00:16 -0400

    added a couple of scripts to analyze the luckiness or hard to predictness of the winner given the availability of various cards

---
+++ oneoff/luck_analyze.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import collections
+import math
+import sys
+sys.path.append('..')
+import stats
+
+def main():
+    confusion_stats = collections.defaultdict(stats.MeanVarStat)
+    for idx, line in enumerate(open('card_luck2.txt')):
+        try:
+            contents = line.split(':')
+            prob = float(contents[4])
+            cards = contents[5].split(',')
+            confusion = -math.log(prob)
+            for card in cards:
+                card = card.strip()
+                confusion_stats[card].add_outcome(confusion)
+        except IndexError:
+            break
+    confusion_stats = confusion_stats.items()
+    confusion_stats.sort(key = lambda x: x[1].mean())
+    for card, stat in confusion_stats:
+        print card, stat.render_interval(sig_digits=3)
+        
+
+if __name__ == '__main__':
+    main()
