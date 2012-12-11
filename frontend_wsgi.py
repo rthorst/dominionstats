0ade8b9fa73d18dc1c8b42a2f512fef69af5f492
Mike McCallister <mike@mccllstr.com>
2012-12-10 21:36:31 -0600

    Removed hard coded path.

--- frontend_wsgi.py
+++ frontend_wsgi.py
@@ -3,7 +3,7 @@ import sys, os
 # this should be in apache config rather rather than code, but I like
 # coding more than I like figuring out apache configuration.  Sorry if it
 # is killing you, feel free to change it for your setup
-os.chdir('/srv/councilroom/councilroom_prod')
+#os.chdir('/srv/councilroom/councilroom_prod')
 
 abspath = os.path.dirname(__file__)
 sys.path.append(abspath)
