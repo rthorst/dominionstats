rthorst/dominionstats

commit 3384b90ef2418fb43cf86fcfa9b0ec47222ca174

Renamed the card module to dominioncards.
This was causing untold pain due to conflicts in variable names. As
the code moved away from using strings like 'Copper' in favor of
instances like dominioncards.Copper, the card module was being
imported into many more places, which often conflicted with local
variables named card.

dominioncards.py
@@ -7,8 +7,8 @@ _cardlist_reader = csv.DictReader(open(
 _CARDS = {}
 _INDEXED = {}
 
-# Returns value of string if the value is unambiguous, otherwise the default
 def int_or_no_int(string, default):
+    """ Returns value of string if the value is unambiguous, otherwise the default"""
     try:
         return int(string)
     except ValueError, e:

frontend.py
@@ -19,7 +19,7 @@ from optimal_card_ratios import DBCardRatioTracker
 from record_summary import RecordSummary
 from small_gain_stat import SmallGainStat
 import annotate_game
-import card
+import dominioncards
 import datetime
 import game
 import goals
@@ -105,7 +105,7 @@ class OpeningPage(object):
 
         results = db.trueskill_openings.find({'_id': {'$regex': '^open:'}})
         openings = list(results)
-        card_list = card.opening_cards()
+        card_list = dominioncards.opening_cards()
         def split_opening(o):
             ret = o['_id'][len('open:'):].split('+')
             if ret == ['']: return []
@@ -124,7 +124,7 @@ class OpeningPage(object):
             opening['skill_str'] = skill_str(opening['mu'], opening['sigma'])
             opening['cards'] = split_opening(opening)
             opening['cards'].sort()
-            opening['cards'].sort(key=card.cost, reverse=True)
+            opening['cards'].sort(key=dominioncards.cost, reverse=True)
             costs = [str(card.cost) for card in opening['cards']]
             while len(costs) < 2:
                 costs.append('-')
@@ -603,7 +603,7 @@ class SupplyWinApi(object):
         # unconditional: opt param, if present, also get unconditional stats.
         targets = query_dict.get('targets', '').split(',')
         if sum(len(t) for t in targets) == 0:
-            targets = card.all_cards()
+            targets = dominioncards.all_cards()
             
         target_inds = map(self.str_card_index, targets)
         interaction_tuples = self.interaction_card_index_tuples(query_dict)
@@ -621,8 +621,8 @@ class OptimalCardRatios(object):
         web.header("Content-Type", "text/html; charset=utf-8")
         query_dict = dict(urlparse.parse_qsl(web.ctx.env['QUERY_STRING']))
 
-        card_list = sorted(set(card.all_cards()) - 
-                           set(card.TOURNAMENT_WINNINGS))
+        card_list = sorted(set(dominioncards.all_cards()) - 
+                           set(dominioncards.TOURNAMENT_WINNINGS))
 
         card_x = query_dict.get('card_x', 'Minion')
         card_y = query_dict.get('card_y', 'Gold')

analysis_util.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import card
+import dominioncards
 import game
 import utils
 
@@ -14,7 +14,7 @@ def available_cards(game_obj, gained_list):
     ret.update(game_obj.get_supply())
     ret.update(card.EVERY_SET_CARDS)
     ret.update(gained_list)
-    if 'Tournament' in ret:
-        ret.update(card.TOURNAMENT_WINNINGS)
+    if dominioncards.Tournament in ret:
+        ret.update(dominioncards.TOURNAMENT_WINNINGS)
     return ret
     

analyze.py
@@ -14,7 +14,7 @@ import simplejson as json
 import sys
 
 import utils
-from card import EVERY_SET_CARDS
+from dominioncards import EVERY_SET_CARDS
 from stats import MeanVarStat
 from game import Game
 import incremental_scanner

analyze2.py
@@ -5,7 +5,7 @@ from __future__ import division
 import os
 
 import analysis_util
-import card
+import dominioncards
 from game import Game
 import incremental_scanner
 import pymongo

count_buys.py
@@ -12,7 +12,7 @@ import pymongo
 
 from stats import MeanVarStat as MVS
 import analysis_util
-from card import EVERY_SET_CARDS
+from dominioncards import EVERY_SET_CARDS
 import game
 import incremental_scanner
 import mergeable

game.py
@@ -9,10 +9,11 @@ import collections
 import itertools
 import pprint
 
-from card import index_to_card, EVERY_SET_CARDS, get_card
+from dominioncards import index_to_card, EVERY_SET_CARDS, get_card
 from keys import *
 from primitive_util import ConvertibleDefaultDict
-import card
+from utils import lru_cache
+import dominioncards
 
 WIN, LOSS, TIE = range(3)
 
@@ -399,18 +400,20 @@ class Game(object):
             s += '%s\n'%str(pd)
         return s
 
+
+@lru_cache
 def score_deck(deck_comp):
     """ Given a dict of cards (as card.Card) and frequency, return the score. """
     ret = 0
-    if card.Gardens in deck_comp:
+    if dominioncards.Gardens in deck_comp:
         ret += score_gardens(deck_comp)
-    if card.Duke in deck_comp:
+    if dominioncards.Duke in deck_comp:
         ret += score_duke(deck_comp)
-    if card.Fairgrounds in deck_comp:
+    if dominioncards.Fairgrounds in deck_comp:
         ret += score_fairgrounds(deck_comp)
-    if card.Vineyard in deck_comp:
+    if dominioncards.Vineyard in deck_comp:
         ret += score_vineyard(deck_comp)
-    if card.SilkRoad in deck_comp:
+    if dominioncards.SilkRoad in deck_comp:
         ret += score_silk_road(deck_comp)
 
     for cardinst in deck_comp:
@@ -420,21 +423,21 @@ def score_deck(deck_comp):
 
 def score_gardens(deck_comp):
     deck_size = sum(deck_comp.itervalues())
-    return deck_size / 10 * deck_comp[card.Gardens]
+    return deck_size / 10 * deck_comp[dominioncards.Gardens]
 
 def score_duke(deck_comp):
-    return deck_comp[card.Duke] * deck_comp.get(card.Duchy, 0)
+    return deck_comp[dominioncards.Duke] * deck_comp.get(dominioncards.Duchy, 0)
 
 def score_fairgrounds(deck_comp):
-    return  2 * (len([count for count in deck_comp.values() if count>0] ) / 5) * deck_comp[card.Fairgrounds]
+    return  2 * (len([count for count in deck_comp.values() if count>0] ) / 5) * deck_comp[dominioncards.Fairgrounds]
 
 def score_vineyard(deck_comp):
     return sum(deck_comp[cardinst] if cardinst.is_action() else 0
-               for cardinst in deck_comp) / 3 * deck_comp[card.Vineyard]
+               for cardinst in deck_comp) / 3 * deck_comp[dominioncards.Vineyard]
 
 def score_silk_road(deck_comp):
     return sum(deck_comp[cardinst] if cardinst.is_victory() else 0
-               for cardinst in deck_comp) / 4 * deck_comp[card.SilkRoad]
+               for cardinst in deck_comp) / 4 * deck_comp[dominioncards.SilkRoad]
 
 class GameState(object):
     def __init__(self, game):
@@ -451,12 +454,12 @@ class GameState(object):
             value_type=lambda: ConvertibleDefaultDict(int))
         self.player_vp_tokens = collections.defaultdict(int)
 
-        self.supply[card.Copper] = self.supply[card.Copper] - (
+        self.supply[dominioncards.Copper] = self.supply[dominioncards.Copper] - (
             len(self.turn_ordered_players) * 7)
 
         for player in self.turn_ordered_players:
-            self.player_decks[player.name()][card.Copper] = 7
-            self.player_decks[player.name()][card.Estate] = 3
+            self.player_decks[player.name()][dominioncards.Copper] = 7
+            self.player_decks[player.name()][dominioncards.Estate] = 3
 
         self.turn_ind = 0
 

game_state_features.py
@@ -5,7 +5,7 @@
 
 import itertools
 
-import card
+import dominioncards
 import game
 import random
 import utils
@@ -15,11 +15,11 @@ def nice_feature_name(n):
 
 def composition_deck_extractor(deck_comp, game_state, player):
     ret = []
-    for card in card.all_cards():
+    for card in dominioncards.all_cards():
         ret.append(deck_comp.get(card, 0))
     return ret
 composition_deck_extractor.feature_names = map(nice_feature_name, 
-                                               card.all_cards())
+                                               dominioncards.all_cards())
 
 def score_deck_extractor(deck_comp, game_state, player):
     return [game_state.player_score(player)]
@@ -30,7 +30,7 @@ def deck_size_deck_extractor(deck_comp, game_state, player):
 def action_balance_deck_extractor(deck_comp, game_state, player):
     ret = 0
     for card, quant in deck_comp.iteritems():
-        ret += (card.num_plus_actions() - card.is_action()) * quant
+        ret += (dominioncards.num_plus_actions() - dominioncards.is_action()) * quant
     return [ret / (sum(deck_comp.itervalues()) or 1)]
 
 def unique_deck_extractor(deck_comp, game_state, player):
@@ -70,10 +70,10 @@ def turn_no_common_extractor(g, game_state):
 
 def supply_common_extractor(g, game_state):
     ret = []
-    for card in card.all_cards():
+    for card in dominioncards.all_cards():
         ret.append(game_state.supply.get(card, 0))
     return ret
-supply_common_extractor.feature_names = map(nice_feature_name, card.all_cards())
+supply_common_extractor.feature_names = map(nice_feature_name, dominioncards.all_cards())
 
 def make_extractor_list(suffix):
     extractor_names = [n[:-len(suffix)] for n in 

commit 38f6335ac87d343a9cd2e6c06b420c180ac092e6

Resolves issues with the `/openings` URL.
Obtain the card costs from the appropriate property in the
`dominioncards` module. This was probably broken by 3384b90e.

On the openings page, handle the change to the openings IDs due to the
change to `__repr__()`.

When generating the card names in the dropdown list on `/openings`,
use the `singular` version instead of `str()`, which implicitly calls
`__repr__()`  (e.g., `Card(Chapel)`).

Enables lookup of cards by their `__repr__()` string.

Eliminate a pylint warning about reusing the `c` variable name in
`dominioncards`.

Closes #31.

dominioncards.py
@@ -102,6 +102,7 @@ def _init():
         c = Card(cardlist_row)
         _CARDS[singular] = c
         _CARDS[plural] = c
+        _CARDS[c.__repr__()] = c      # Enable lookup by __repr__()
         _INDEXED[c.index] = c
 
 _init()
@@ -109,9 +110,9 @@ _init()
 def all_cards():
     return _INDEXED.values()
 
-for c in all_cards():
-    pname = PythonifyName(c.singular)
-    vars()[ pname ] = c
+for init_card in all_cards():
+    pname = PythonifyName(init_card.singular)
+    vars()[ pname ] = init_card
 
 def get_card(name):
     """ Look up a card by its name """

frontend.py
@@ -109,7 +109,10 @@ class OpeningPage(object):
         def split_opening(o):
             ret = o['_id'][len('open:'):].split('+')
             if ret == ['']: return []
-            return ret
+
+            # Convert the __repr__() representation stored in the
+            # database to the singular version of the card name.
+            return [dominioncards.get_card(card).singular for card in ret]
 
         if selected_card not in ('All cards', ''):
             openings = [o for o in openings if selected_card in 
@@ -124,8 +127,8 @@ class OpeningPage(object):
             opening['skill_str'] = skill_str(opening['mu'], opening['sigma'])
             opening['cards'] = split_opening(opening)
             opening['cards'].sort()
-            opening['cards'].sort(key=dominioncards.cost, reverse=True)
-            costs = [str(card.cost) for card in opening['cards']]
+            opening['cards'].sort(key=lambda card: dominioncards.get_card(card).cost, reverse=True)
+            costs = [str(dominioncards.get_card(card).cost) for card in opening['cards']]
             while len(costs) < 2:
                 costs.append('-')
             opening['cost'] = '/'.join(costs)

openings_template.html
@@ -36,9 +36,9 @@ particular card or "All cards".
 <option>All cards</option>
 $for card in card_list:
     $if card == selected_card:
-        <option selected>$card</option>
+        <option selected>$card.singular</option>
     $else:
-        <option>$card</option>
+        <option>$card.singular</option>
 </select>
 <input type="submit" value="Go">
 </form>