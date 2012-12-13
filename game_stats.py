eca8a9608833dc06083d8e719ce1265b71949df0
Mike McCallister <mike@mccllstr.com>
2012-12-13 00:35:38 -0600

    Worked over the game_stats.py module.
    Use module-level logging, initialized by the standard function when
    invoked from the command line. Avoid passing the log objects around as
    it makes it harder to figure out where the messages came from.
    
    Take player names on the command line.
    
    Avoided the wildcard import of keys to comply with best practices.

--- game_stats.py
+++ game_stats.py
@@ -1,23 +1,32 @@
-import logging
-import utils
-import game
-from keys import *
-from name_merger import norm_name
+"""Figure out per-player statistics in advance"""
 
-def calculate_game_stats(log, games, game_stats):
+import logging
+
+from name_merger import norm_name
+import dominionstats.utils.log
+import game
+import keys
+import utils
+
+
+# Module-level logging instance
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+def calculate_game_stats(games, game_stats):
     """ Save game statistics for each game and player
 
-    log: Logging Object
     games: List of games to analyze, each in dict format
     game_stats: Destination MongoDB collection
     """
     log.debug("Beginning to analyze game statistics for %d games", len(games))
-    game_stats.ensure_index(NAME)
+    game_stats.ensure_index(keys.NAME)
     for game_dict in games:
         game_val = game.Game(game_dict)
         g_id = game_dict['_id']
         date = game_dict['game_date']
-        supply = game_dict[SUPPLY]
+        supply = game_dict[keys.SUPPLY]
 
         for p in get_game_stat_entries(game_val, g_id, date, supply):
             game_stats.save(p)
@@ -32,29 +41,38 @@ def get_game_stat_entries(game_val, g_id, date, supply):
         m = {}
         name = norm_name(full_name)
         m['_id'] = "%s/%s" % (g_id, name)
-        m[NAME] = name
-        m[PLAYERS] = [p for p in all_p if p != full_name]
+        m[keys.NAME] = name
+        m[keys.PLAYERS] = [p for p in all_p if p != full_name]
         m['game_date'] = date
 
         pd = game_val.get_player_deck(full_name)
-        m[WIN_POINTS] = pd.WinPoints()
-        m[RESULT] = game_val.win_loss_tie(full_name)
-        m[ORDER] = pd.TurnOrder()
-        m[SUPPLY] = supply
+        m[keys.WIN_POINTS] = pd.WinPoints()
+        m[keys.RESULT] = game_val.win_loss_tie(full_name)
+        m[keys.ORDER] = pd.TurnOrder()
+        m[keys.SUPPLY] = supply
         ret.append(m)
 
     return ret
 
-if __name__=='__main__':            
-    log = logging.getLogger(__name__)
+
+def main(args):
     db = utils.get_mongo_database()
     games = db.games
     game_stats = db.game_stats
 
-    norm_target_player = 'hawaiian shirts'
-    games_coll = games.find({PLAYERS: norm_target_player})
+    for player_name in args.players:
+        log.debug("Processing top level player name %s", player_name)
+        norm_target_player = norm_name(player_name)
+        games_coll = games.find({keys.PLAYERS: norm_target_player})
 
-    calculate_game_stats(log, list(games_coll), game_stats)
+        calculate_game_stats(list(games_coll), game_stats)
 
 
-
+if __name__=='__main__':
+    parser = utils.base_parser()
+    parser.add_argument(
+        '--players', nargs='+',
+        help='Check only the player(s) specified')
+    parsed_args = parser.parse_args()
+    dominionstats.utils.log.initialize_logging(parsed_args.debug)
+    main(parsed_args)
